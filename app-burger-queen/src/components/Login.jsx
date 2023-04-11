98fb7a4e30ae065c7a8cbce90e27ac71dfc72261
Andry Salazar <avesalazar4@gmail.com>
2023-04-11 12:32:03 -0500

    feat: boilerplate, instalación react and bootstrap

---
+++ app-burger-queen/src/components/Login.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div>
+    <h1>Burger Queen</h1>
+    <input type="text" placeholder="Correo electronico"></input>
+    <div>
+    <input type="text" placeholder="Contraseña"></input>
+    </div>
+    </div>
+  );
+}
+
+export default Login;
