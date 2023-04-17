58127cbd10c7e95bebbe729d277a3d4f44f682d2
sara040616 <sara.sierraramos@gmail.com>
2023-04-17 14:08:37 -0500

    validando email y password

--- app-burger-queen/src/components/Login.jsx
+++ app-burger-queen/src/components/Login.jsx
@@ -8,11 +8,23 @@ import Col from 'react-bootstrap/Col';
 import { useForm } from 'react-hook-form';
 
 function Login() {
-  const { register, handleSubmit, formState : {errors }} = useForm();
+  const { register, handleSubmit} = useForm({ mode: "onChange"});
+  
 
   const onSubmit = evento => {
-    
-  }
+    console.log(evento)
+  } 
+  const messages = {
+    req: "Este campo es obligatorio",
+    email: "El formato de correo electrónico no es válido",
+    password: "Debes introducir una contraseña válida"
+   };
+
+   const patterns = {
+    email : /ˆ[A-Z0-9 ._%+-]+@<A-Z0-9.-+\.[A-Z]{2,4}$/i, 
+    password: /^[0-9]+$/i,
+   }
+   
 
   return (
     <Container>
@@ -21,29 +33,40 @@ function Login() {
         <div className="titulo">
           <h1 className="mb-5">Burger Queen</h1>
         </div>
+        
       <Form onSubmit = {handleSubmit(onSubmit)}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           {/* <Form.Label>Correo electrónico</Form.Label> */}
-          <Form.Control name="email" type="email" placeholder="Correo electrónico" {...register('email', {required :
-          {
-            value: true,
-            message: 'El campo es requerido',
-            pattern:{
-              value: /ˆ[A-Z0-9 ._%+-]+@<A-Z0-9.-+\.[A-Z]{2,4}$/i,
-              message: 'El formato de correo electrónico no es válido'
+          <Form.Control name="email" type="email" placeholder="Correo electrónico" {...register('email', {
+            required : messages.req,
+            pattern : {
+              value : patterns.email,
+              message: messages.email
             }
-          }})}/>
+            
+          })
+          }/>
         </Form.Group>
         
         <Form.Group className="mb-3" controlId="formBasicPassword">
           {/* <Form.Label>Contraseña</Form.Label> */}
-          <Form.Control name="password" type="password" placeholder="Contraseña" {...register('password', {required:{
-            value: true,
-            message: 'El campo es requerido',
+          <Form.Control name="password" type="password" placeholder="Contraseña" {...register('password', {
+            required:messages.req,
               minLength:{
               value: 6,
-              message: 'La contraseña debe contener al menos 6 carácteres'
-            }}})}/> 
+              message: messages.password
+            },
+            maxLength: {
+              value: 16,
+              message: messages.password
+            },
+            pattern: {
+              value: patterns.password,
+              message: messages.password
+            }
+            }
+            )}
+            /> 
         </Form.Group>
        
         <Button type='submit' size="lg" variant="danger">Iniciar Sesión</Button>
