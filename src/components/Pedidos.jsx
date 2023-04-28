a1768ee01680897a4cea058972a0aa456d70a477
mariahiguera2210 <angelica.higueram92@gmail.com>
2023-04-28 11:02:36 -0500

    se hace ruteo

---
+++ src/components/Pedidos.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Pedidos = () => {
+
+    return (<><div className="card text-white bg-primary mb-3" style= {{ maxWidth: "18rem" , background : "#E6AF2E", borderRadius : "15px", padding : "15px"}}>
+        <div className="card-header">Header</div>
+        <div className="card-body">
+            <h5 className="card-title">Primary card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+    </div>
+        
+        </>)
+
+}
+
+export default Pedidos;
