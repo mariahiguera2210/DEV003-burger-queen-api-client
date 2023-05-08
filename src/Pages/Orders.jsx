9ba4a3fb0864b737c3ffa0103115ea237c6a3288
mariahiguera2210 <angelica.higueram92@gmail.com>
2023-05-08 11:04:18 -0500

    api-mock

--- src/Pages/Orders.jsx
+++ src/Pages/Orders.jsx
@@ -1,33 +1,28 @@
-import React, {useState, useEffect}from "react";
+import React, { useState, useEffect } from "react";
 import OrderCard from "../components/OrderCard";
 
-
 const Orders = () => {
-   
-    const [orders, setOrders] = useState([])
-    
-        useEffect(() => {
-        fetch("data/orders.json")
-            .then(response => response.json())
-            .then(data => setOrders(data['orders']))
-            .catch(error => console.error(error))
-      }, [])
-      
-      // console.log(orders)
-      
-    return (
-      <>
-      <h1 className="titleOrders">Ordenes Pendientes</h1>
-        {orders.map(order => (
-          <OrderCard
-          order={order} 
-          key={order.id}
-          
-         />))}
-      </>
-    
-        )
+  const [orders, setOrders] = useState([]);
 
-}
+  useEffect(() => {
+    fetch("data/orders.json")
+      .then((response) => response.json())
+      .then((data) => setOrders(data["orders"]))
+      .catch((error) => console.error(error));
+  }, []);
+
+  const pendingOrders = orders.filter((order) => order.status === "pending");
+ 
+
+  return (
+    <>
+      <h1 className="titleOrders">Ordenes Pendientes</h1>
+      {pendingOrders.map((order) => (
+        <OrderCard order={order} key={order.id} />
+      ))}
+
+    </>
+  );
+};
 
 export default Orders;
