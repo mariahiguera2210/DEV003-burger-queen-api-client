cff604de9fc6e1685a8cca6f40600b59d07d3dee
mariahiguera2210 <angelica.higueram92@gmail.com>
2023-05-09 10:51:50 -0500

    se hacen peticiones fetch en login y se guarda token el locastorage

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState  } from "react";
 import Button from 'react-bootstrap/Button';
 import ErrorMessage from "./ErrorMessage";
-import AuthService from "./AuthService";
+// import AuthService from "./AuthService";
 import { useNavigate } from "react-router-dom";
 import Form from 'react-bootstrap/Form';
 
@@ -11,31 +11,40 @@ const LoginForm = () => {
   const [error, setError] = useState("");
   const navigate = useNavigate();
 
+
   const handleSubmit = (e) => {
     e.preventDefault();
     
-    AuthService.authenticate(email, password)
-      .then((isAuthenticated) => {
-       if(isAuthenticated){
-       navigate("/menu");
-       }else{
-        console.log({error});
-       }
+    fetch('http://localhost:8080/login', { method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password }),})
+      .then((response) => response.json())
+      .then((data) => { 
+        const accessToken= data.accessToken
+        console.log('accessToken: ', accessToken);
+       
+        const user = data.user.email
+        localStorage.setItem("sesionUser", JSON.stringify(user));
+        localStorage.setItem("sesionToken", accessToken);
+        navigate("/menu")
       })
+
       .catch((error) => {
-        setError(error.message);
-        
+        console.error(error);
       });
+  
   };
 
   return (
     <Form onSubmit={handleSubmit}>
-         <Form.Group className="mb-6" label="Correo electrónico"
+         <Form.Group className="mb-6" label="Correo electrónico" 
         type="email"
         value={email}
         onChange={(e) => setEmail(e.target.value)}>
         <Form.Label className="">Email</Form.Label>
-        <Form.Control type="email" placeholder="Enter email"/>
+        <Form.Control type="email" placeholder="Enter email" autoComplete = "username"/>
         <Form.Text className="text-muted">
         {error && <ErrorMessage message={"email no válido"} />}
         </Form.Text>
@@ -47,11 +56,11 @@ const LoginForm = () => {
         onChange={(e) => setEmail(e.target.value)}
       /> */}
       <Form.Group className="mb-3" controlId="formBasicPassword"         label="Contraseña"
-        type="password"
+        type="password" 
         value={password}
         onChange={(e) => setPassword(e.target.value)}>
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control type="password" placeholder="Password" autoComplete= "current-password"/>
       </Form.Group>
       {/* <Input
         label="Contraseña"
@@ -60,7 +69,7 @@ const LoginForm = () => {
         onChange={(e) => setPassword(e.target.value)}
       /> */}
        <Button variant="primary" type="submit">
-        Submit
+        Login
       </Button>
       {error && <ErrorMessage message={error} />}
     </Form>
