82ac1b188990166306b50e5eef9992194c1d3761
sara040616 <sara.sierraramos@gmail.com>
2023-05-15 21:48:28 -0500

    Feat: Arreglos en el navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,48 +8,47 @@ import { BiLogOut } from 'react-icons/bi';
 
 function Header() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" style={{ fontFamily: "'Lilita One', cursive" }}>
       <Container>
         <Navbar.Brand style={{ fontSize: '2rem', color: '#E6AF2E' }}>Burger Queen</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto navbar-nav" style={{ fontSize: "1.2rem"}}>
-            <Nav>
-              <Link to="/" 
-              style={{
+          <Nav className="me-auto navbar-nav" style={{ fontSize: "1.2rem" }}>
+              <Nav>
+                <Link to="/menu" style={{
                   textDecoration: 'none',
                   color: 'var(--bs-navbar-color)',
-                }}>Login</Link>
-            </Nav>
-            <Nav>
-              <Link to="/menu"
-               style={{
-                  textDecoration: 'none',
-                  color: 'var(--bs-navbar-color)',
+                  margin: '10px',
                 }}>Menu</Link>
-            </Nav>
-            <Nav>
-              <Link to="/orders" 
-              style={{
+              </Nav>
+              <Nav>
+                <Link to="/orders" style={{
                   textDecoration: 'none',
                   color: 'var(--bs-navbar-color)',
+                  margin: '10px',
                 }}>Ordenes</Link>
-            </Nav>
+              </Nav>
+              <Nav>
             <NavDropdown title="Administrador" id="collasible-nav-dropdown">
               <NavDropdown.Item>
                 <Link to="/products" style={{
-                  textDecoration: 'none', color:'black'}}>Productos</Link>
+                  textDecoration: 'none',
+                  color: 'black'
+                }}>Productos</Link>
               </NavDropdown.Item>
-              <NavDropdown.Item >
+              <NavDropdown.Item>
                 <Link to="/empleados" style={{
                   textDecoration: 'none',
-                  color:'black'}}>Empleados</Link>
+                  color: 'black'
+                }}>Empleados</Link>
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
-          <Nav>
-            <Nav.Link to="/"><BiLogOut /></Nav.Link>
           </Nav>
+          <Nav >
+            <Link to='/' style={{ textDecoration: 'none', color: '#9B9D9E', fontSize: '30px'}}><BiLogOut /></Link>
+          </Nav>
+         
         </Navbar.Collapse>
       </Container>
     </Navbar>
