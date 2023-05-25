4fb3c02b0f6b9e728aa5863d6d107daa7e0d1f21
sara040616 <sara.sierraramos@gmail.com>
2023-05-25 10:17:56 -0500

    Ajuste de diseño

--- src/components/carrito/TableOrders.jsx
+++ src/components/carrito/TableOrders.jsx
@@ -1,12 +1,12 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import Badge from 'react-bootstrap/Badge';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Image from 'react-bootstrap/Image';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
-import {AiOutlineLine} from 'react-icons/ai';
-import {GrAdd} from 'react-icons/gr';
+import { AiOutlineLine } from 'react-icons/ai';
+import { GrAdd } from 'react-icons/gr';
 import { CartContext } from './CartContext';
 
 function TableOrders() {
@@ -14,12 +14,12 @@ function TableOrders() {
   return (
     <Row>
       <ListGroup className="align-items-center">
-          {cart.map((products) =>
-
-          <ListGroup.Item as="li"
-              className="d-flex justify-content-between align-items-start position-relative"
-              key={products.id} >
-
+        {cart.map((products) => (
+          <ListGroup.Item
+            as="li"
+            className="d-flex justify-content-between align-items-start position-relative"
+            key={products.id}
+          >
             <Col xs={2} md={2}>
               <Badge bg="warning" pill className="position-absolute top-0 start-0">
                 {products.qty}
@@ -27,31 +27,31 @@ function TableOrders() {
               <Image src={products.image} alt={products.name} thumbnail />
             </Col>
 
-            <Col xs={7} md={7} style={{ paddingTop: '8px' }}>
-              <p style={{ marginBottom: '3px' }}> <strong>{products.name} </strong></p>
-              <p style={{ marginBottom: '3px' }}>{products.type}</p>
+            <Col xs={7} md={7} style={{ paddingTop: '2%', marginLeft: '10px' }}>
+              <p style={{ marginBottom: '3px', fontSize: '15px' }}>
+                <strong>{products.name}</strong>
+              </p>
+              <p style={{ marginBottom: '3px', fontSize: '13px'}}>{products.type}</p>
             </Col>
 
-            <Col xs={3} md={3} className="text-end"style={{ 
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-              marginTop: '3%'}}>
-
-              <Button variant="light">
+            <Col
+              xs={3}
+              md={3}
+              className="d-flex justify-content-end align-items-center"
+              style={{ marginTop: '2%' }}
+            >
+              <Button variant="outline-warning" style={{ marginRight: '10px' }}>
                 <AiOutlineLine />
               </Button>
 
-              <p>{products.price}</p>
+              <p style={{ marginTop: '15%', marginRight: '10px' }}>${products.price}</p>
 
-              <Button variant="light">
+              <Button variant="outline-warning">
                 <GrAdd />
               </Button>
-           </Col>
-
+            </Col>
           </ListGroup.Item>
-       )}
+        ))}
       </ListGroup>
     </Row>
   );
