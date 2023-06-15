ab195cefb7c6176c531a596f53d09243f9ba6c8f
mariahiguera2210 <angelica.higueram92@gmail.com>
2023-06-15 18:55:18 -0500

    npm run build

---
+++ build/precache-manifest.56da3b008cc1faad419dd58361ce4922.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "71a3e8b37321a46b0790",
+    "url": "/static/css/main.93792306.chunk.css"
+  },
+  {
+    "revision": "71a3e8b37321a46b0790",
+    "url": "/static/js/main.b4f119b7.chunk.js"
+  },
+  {
+    "revision": "814fac06a890e7c45643",
+    "url": "/static/js/runtime~main.6360dd90.js"
+  },
+  {
+    "revision": "cf2df12e041efa75e9d8",
+    "url": "/static/css/2.30f3a6f6.chunk.css"
+  },
+  {
+    "revision": "cf2df12e041efa75e9d8",
+    "url": "/static/js/2.56a59956.chunk.js"
+  },
+  {
+    "revision": "8d288c4508d80842dba8",
+    "url": "/static/js/3.04f07501.chunk.js"
+  },
+  {
+    "revision": "02711ac11c11eb02d0009bcf541c5954",
+    "url": "/index.html"
+  }
+];
