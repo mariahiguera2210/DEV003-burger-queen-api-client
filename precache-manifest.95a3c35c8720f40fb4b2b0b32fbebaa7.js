8fec09dbae93bfcb688092b0baa1015b2bd1ce7e
mariahiguera2210 <angelica.higueram92@gmail.com>
2023-06-15 19:32:30 -0500

    Updates

---
+++ precache-manifest.95a3c35c8720f40fb4b2b0b32fbebaa7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "71a3e8b37321a46b0790",
+    "url": "/DEV003-burger-queen-api-client/static/css/main.93792306.chunk.css"
+  },
+  {
+    "revision": "71a3e8b37321a46b0790",
+    "url": "/DEV003-burger-queen-api-client/static/js/main.b4f119b7.chunk.js"
+  },
+  {
+    "revision": "d177a6a313f335f685bb",
+    "url": "/DEV003-burger-queen-api-client/static/js/runtime~main.b9148ccd.js"
+  },
+  {
+    "revision": "cf2df12e041efa75e9d8",
+    "url": "/DEV003-burger-queen-api-client/static/css/2.30f3a6f6.chunk.css"
+  },
+  {
+    "revision": "cf2df12e041efa75e9d8",
+    "url": "/DEV003-burger-queen-api-client/static/js/2.56a59956.chunk.js"
+  },
+  {
+    "revision": "8d288c4508d80842dba8",
+    "url": "/DEV003-burger-queen-api-client/static/js/3.04f07501.chunk.js"
+  },
+  {
+    "revision": "0bd815e84968f6c6f12193620c7e236d",
+    "url": "/DEV003-burger-queen-api-client/index.html"
+  }
+];
