0a4a6a477cdd7a39f34d737019f66d9d63422baf
mariahiguera2210 <angelica.higueram92@gmail.com>
2023-06-15 19:33:47 -0500

    Deploy React app to GitHub Pages

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.56da3b008cc1faad419dd58361ce4922.js"
+  "/DEV003-burger-queen-api-client/precache-manifest.95a3c35c8720f40fb4b2b0b32fbebaa7.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/DEV003-burger-queen-api-client/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
