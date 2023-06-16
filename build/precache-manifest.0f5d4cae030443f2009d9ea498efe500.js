0f9387deb376722dfa39b361d9789f8cf6de1760
mariahiguera2210 <angelica.higueram92@gmail.com>
2023-06-15 20:56:53 -0500

    deploy

---
+++ build/precache-manifest.0f5d4cae030443f2009d9ea498efe500.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "71a3e8b37321a46b0790",
+    "url": "./static/css/main.93792306.chunk.css"
+  },
+  {
+    "revision": "71a3e8b37321a46b0790",
+    "url": "./static/js/main.b4f119b7.chunk.js"
+  },
+  {
+    "revision": "e9edb60097993712ff59",
+    "url": "./static/js/runtime~main.889e125e.js"
+  },
+  {
+    "revision": "cf2df12e041efa75e9d8",
+    "url": "./static/css/2.30f3a6f6.chunk.css"
+  },
+  {
+    "revision": "cf2df12e041efa75e9d8",
+    "url": "./static/js/2.56a59956.chunk.js"
+  },
+  {
+    "revision": "8d288c4508d80842dba8",
+    "url": "./static/js/3.04f07501.chunk.js"
+  },
+  {
+    "revision": "d1978e262cabdccc2d83bcb5abcb6fe7",
+    "url": "./index.html"
+  }
+];
